35c49180d8cac66ff8cb4098b7a5321b56a436e6
Walter Schweitzer <wschweitzer00@gmail.com>
2018-11-07 11:42:47 -0500

    more install steps

--- courses/courses/settings.py
+++ courses/courses/settings.py
@@ -75,9 +75,10 @@ WSGI_APPLICATION = 'courses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'courses',
     }
+    ## Add other data storage here
 }
 
 
