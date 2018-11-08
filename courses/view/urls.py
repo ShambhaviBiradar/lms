7d47a7dd192874f6a4eadfe786a9350975514715
Walter Schweitzer <wschweitzer00@gmail.com>
2018-11-08 13:53:47 -0500

    added jwplayer video template

--- courses/view/urls.py
+++ courses/view/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('test-video', views.test_video, name='test_video'),
 ]
--- courses/view/views.py
+++ courses/view/views.py
@@ -2,7 +2,11 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 
 def index(request):
     return HttpResponse("Here are the codez.")
+
+def test_video(request):
+    return render(request, 'videos/index.html')
